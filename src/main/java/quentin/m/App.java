71e56d2a3402b70b0f7aae304c2778dabc99e8c5
quentinmathieu <quentin.mth@gmail.com>
2024-06-05 16:10:18 +0200

    Print name in a frame, decomposed letter by letter

---
+++ src/main/java/quentin/m/App.java
@@ -0,0 +1,115 @@
+package quentin.m;
+
+import java.io.IOException;
+import java.util.Scanner;
+
+import org.jline.terminal.Terminal;
+import org.jline.terminal.TerminalBuilder;
+
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args ) throws IOException
+    {
+         // init reader for user prompt
+        Scanner reader = new Scanner(System.in);
+
+        clearScreen();
+        System.out.println("Enter a string");
+        String sentence = reader.nextLine();
+        int[] terminalSize = getTerminalSize();
+
+
+        printString(terminalSize, sentence);
+
+
+        // close de prompt's reader
+        reader.close();
+    }
+
+
+    //  get the width & the height of the terminal
+    public static int[] getTerminalSize() throws IOException
+    {
+        Terminal terminal = TerminalBuilder.terminal();
+        int terminalWidth = terminal.getWidth();
+        int terminalHeight = terminal.getHeight();
+
+        return new int[]{terminalWidth, terminalHeight};
+    }
+
+
+    // Print on the screen step by step the user's input
+    public static void printString(int[] terminalSize, String sentence) {
+        
+
+        for(int i = 0; i<=sentence.length();i++){
+            String newSentence = sentence.substring(0, i); 
+            printScreen(terminalSize, newSentence, sentence.length());
+            try {
+                Thread.sleep(400);
+            } catch(InterruptedException e) {
+                System.out.println("got interrupted! ");
+            }
+        }
+
+
+    }
+
+    // Print a "frame" 
+    public static void printScreen(int[] terminalSize, String sentence,  int sentenceLength) 
+    {
+        clearScreen();
+        int terminalWidth = terminalSize[0];
+        // int terminalHeight = terminalSize[1]-7;
+        int terminalHeight = 7;
+        for (int i=0; i < terminalHeight; i++){
+            String strLine = "";
+            // 
+
+            for (int j=0; j < terminalWidth; j++){
+                if (j == 0 && i ==0){
+                    strLine += "╔";
+                }
+                else if (j == terminalWidth-1 && i ==0){
+                    strLine += "╗";
+                }
+                else if (j == 0 && i ==terminalHeight-1){
+                    strLine += "╚";
+                }
+                else if (j == terminalWidth-1 && i ==terminalHeight-1){
+                    strLine += "╝";
+                }
+                else if (i==0 || i == terminalHeight-1){
+                    strLine += "═";
+                }
+                else if (j==0 || j== terminalWidth-1) {
+                    strLine += "║";
+                }
+                // print the sentence
+                else if((j >= (terminalWidth/2)-(sentenceLength/2) && i == terminalHeight/2) && sentence.length()>0){
+                    strLine += (sentenceLength-sentence.length()>0) ? sentence + "▒" : sentence + " ";
+                    for(int index = 0; index < sentenceLength-sentence.length(); index++){
+                        strLine += " ";
+                    }
+                    j+=sentenceLength;
+                    sentence = "";
+                }
+                else{
+                    strLine += " ";
+                }
+            }
+            System.out.println(strLine);
+            
+        }
+    }
+
+    public static void clearScreen() {
+        System.out.print("\033[H\033[2J");
+        System.out.flush();
+    }
+}
